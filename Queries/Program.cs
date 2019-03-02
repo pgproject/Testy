c8926664e2a5f005d14730b6170cef5ef652c5ee
pgproject <grzp91@gmail.com>
2019-03-02 15:54:16 +0100

    Test

---
+++ Queries/Program.cs
@@ -0,0 +1,14 @@
+﻿using Queries.Persistence;
+using System;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        }
+    }
+}
+  
---
+++ TestQueries/UnitOfWorkTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Queries.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestQueries
+{
+    [TestFixture]
+    public class UnitOfWorkTest
+    {
+        [Test]
+        public void UnitOfWorkTestContext()
+        {
+            using (var unitOfWork = new UnitOfWork(new PlutoContext()))
+            {
+                //var course = unitOfWork.Courses.Get(1);
+                //Assert.AreNotEqual(course, 1);
+
+                var course1 = unitOfWork.Authors.GetAuthorWithCourses(1);
+                Assert.AreEqual(course1, 1);
+            }
+        }
+        [Test]
+        public void UnitOfWorkTestContext1()
+        {
+            using (var unitOfWork = new UnitOfWork(new PlutoContext()))
+            {
+                var course = unitOfWork.Courses.Get(1);
+                Assert.AreNotSame(course, "1");
+            }
+        }
+
+        [Test]
+        public void UnitOfWorkTestContext2()
+        {
+            using (var unitOfWork = new UnitOfWork(new PlutoContext()))
+            {
+                var course = unitOfWork.Courses.GetTopSellingCourses(100);
+                Assert.IsNull(course );
+            }
+        }
+    }
+    
+}
